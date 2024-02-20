3e5af8fc1e5a186342ab3ed870c642b895f17143
davidddx <david0319072@gmail.com>
2024-02-20 10:00:56 -0500

    Added logger

---
+++ debug/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+logfile = "projectDebug.log"
+with open(logfile, 'w'):  # truncates file if there is contents in it
+    pass
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG
+                )
+handler = logging.FileHandler(logfile)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+try:
+    logger.info('logger has been initialized')
+except Exception as e:
+    print(f"Error during logging: {e}")
