77c8d888a644c6c56aeffacfde686515176f33f8
davidddx <david0319072@gmail.com>
2024-02-22 15:37:18 -0500

    testing

--- game/Game.py
+++ game/Game.py
@@ -1,18 +1,22 @@
 from debug.logger import logger
 import pygame
-from SceneHandler import SceneHandler
+from game.SceneHandler import SceneHandler
+import os
 class Game:
     def __init__(self):
         try:
             logger.debug(f"Class {Game=} initializing....")
             self.sceneHandler = SceneHandler();
+            cwd = os.getcwd()
+            imagedir = cwd + '/images/test/YellowBlock.png'
+            self.testImage = pygame.image.load(imagedir)
 
             pass
 
             logger.debug(f"Class {Game=} intialized.")
         except Exception as e:
-            logger.debug(f"Failed {Game=} class initialization.\n Error: {e}")
+            logger.error(f"Failed {Game=} class initialization.\n Error: {e}")
 
     def run(self, screen : pygame.Surface):
-        screen.blit()
+        self.sceneHandler.run()
 
