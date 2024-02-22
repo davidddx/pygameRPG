1b9c8f55ed5a2bda7850bf0ba370fd57e439913a
davidddx <david0319072@gmail.com>
2024-02-22 15:37:44 -0500

    added update

--- game/Player.py
+++ game/Player.py
@@ -1,5 +1,6 @@
 import pygame
 from debug.logger import logger
+import globalVars.constants as globalVars
 
 class Player:
     def __init__(self, pos : tuple[int,int,int], head_img : pygame.Surface, shirt_img : pygame.Surface, pants_img : pygame.Surface):
@@ -15,3 +16,7 @@ class Player:
 
         except Exception as e:
             logger.info(f"Failed to intialize class {Player=}.\n Error: {e}")
+    def update(self):
+        globalVars.SCREEN.blit(self.head, (self.pos[0], self.pos[1] + self.shirt.get_height()))
+        globalVars.SCREEN.blit(self.shirt, (self.pos[0], self.pos[1]))
+        globalVars.SCREEN.blit(self.pants, (self.pos[0], self.pos[1] - self.shirt.get_height()))
