876f19cf10a4e75f0e22c6211550085c330583d7
davidddx <david0319072@gmail.com>
2024-02-24 21:14:55 -0500

    renamed

--- game/Area.py
+++ game/Area.py
@@ -1,21 +1,25 @@
 from game.Player import Player as Player
 from debug.logger import logger
 import game.utils.TileMap as TileMap
-class Level:
+class Area:
     def __init__(self, map : TileMap):
         self.map=None
         try:
-            logger.debug(f"Class {Level=} initializing....")
+            logger.debug(f"Class {Area=} initializing....")
             # self.player = Player();
             self.map = map
-            logger.debug(f"Class {Level=} intialized.")
+            logger.debug(f"Class {Area=} intialized.")
         except Exception as e:
-            logger.error(f"Failed {Level=} class initialization.\n Error: {e}")
+            logger.error(f"Failed {Area=} class initialization.\n Error: {e}")
 
     def update(self, screen):
         # self.player.update();
         self.displayMap(map=self.map, screen=screen)
 
+    def prepareForSceneSwitching(self):
+        self.map.collidableSpriteGroup.empty()
+        self.map.nonCollidableSpriteGroup.empty()
+        del self.map
     def displayMap(self, map : TileMap, screen):
         for tile in map.nonCollidableSpriteGroup:
             screen.blit(tile.image, (tile.rect.x, tile.rect.y))
