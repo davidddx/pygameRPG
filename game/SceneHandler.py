d4b165fd2a53fd7157e3e2c8237c82916a25eb49
davidddx <david0319072@gmail.com>
2024-02-26 19:19:44 -0500

    no more map loading in scene handler

--- game/SceneHandler.py
+++ game/SceneHandler.py
@@ -1,7 +1,7 @@
 from debug.logger import logger
 import globalVars.PathConstants as PATH_CONSTANTS
 from game.TileMap import TileMap
-from game.Area import Area
+from game.Scenes.Area import Area
 import os
 import importlib
 import pygame
@@ -16,54 +16,21 @@ class SceneHandler:
         self.timeLastChangedScene = pygame.time.get_ticks()
         try:
             logger.debug(f"Class {SceneHandler=} initializing....")
-            self.Areas = self.getAreas()
-            self.scenes = self.Areas
-            self.SCENE_COUNT = len(self.Areas)
-            logger.debug(f"{self.SCENE_COUNT=}")
-            self.currentSceneIndex = SAVED_DATA.CURRENT_SCENE_INDEX
-            self.currentScene = self.Areas[self.currentSceneIndex]
+            self.scenes = self.loadScenes();
+            self.currentScene = self.loadCurrentScene(_type = SAVED_DATA.CURRENT_SCENE_TYPE, index= SAVED_DATA.CURRENT_SCENE_INDEX);
             logger.debug(f"Class {SceneHandler=} intialized.")
         except Exception as e:
             logger.error(f"Failed {SceneHandler=} class initialization.\n Error: {e}")
 
-    def getAreas(self):
-        def getTileMaps():
-            maps = []
-            cwd = os.getcwd();
-            file = None
-            try:
-                logger.debug(f"Importing scenes...")
-                mapsDirectory = os.path.join(cwd, 'gamedata/Maps')
-                logger.debug(f"Mapsdirectory: {os.listdir(mapsDirectory)=}")
-                for folder in os.listdir(mapsDirectory):
-                    folderPath = os.path.join(os.path.join(mapsDirectory, folder))
-                    for fileName in os.listdir(folderPath):
-                        if fileName.endswith(".py") and not fileName.startswith("__"):
-                            mapModuleName = fileName[:-3]  # Remove the ".py" extension
-                            mapModulePath = f'{PATH_CONSTANTS.GAME_DATA}.{PATH_CONSTANTS.MAPS}.{folder}.{mapModuleName}'
-                            logger.debug(f"Fetching maps from {folder=} \n Current Map: {mapModulePath=}")
-                            try:
+    def getTitleScreen(self):
+        pass
 
-                                mapModule = importlib.import_module(mapModulePath)
-                                logger.debug(f"Loading map module: {mapModule=}")
-                                tileMap = TileMap(tile_set=mapModule.TileSet, tile_map=mapModule.Map)
-                                maps.append(tileMap)
-                                logger.debug(f"Loaded map module: {mapModule}")
-                            except Exception as e:
-                                file_path = os.path.abspath(os.path.join(mapsDirectory, folder))
-                                logger.error(f"Failed to load scene module {mapModulePath} in file {file_path}: {e}")
-                # modulePath = Directory
-                # file = importlib.import_module(modulePath)
-            except Exception as e:
-                logger.error(f"Failed to import scenes.\nException: {e}")
-            scenes = tuple(maps)
+    def loadScenes(self):
+        pass
 
-            return scenes
-
-        areas = []
-        maps = getTileMaps()
-        areas.append(Area(maps=maps))
-        return tuple(areas)
+    def loadCurrentScene(self, _type: str, index : int):
+        if _type == PATH_CONSTANTS.AREA:
+            return Area(map_idx= index)
 
     def run(self, screen):
         self.currentScene.update(screen=screen)
