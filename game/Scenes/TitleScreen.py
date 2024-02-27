c39505202fab05497b5861203262d507fde0b83d
davidddx <david0319072@gmail.com>
2024-02-26 19:25:30 -0500

    framework for TitleScreen scene

---
+++ game/Scenes/TitleScreen.py
@@ -0,0 +1,16 @@
+from game.Scenes.BaseScene import Scene
+from game.utils.Button import ImagedButton
+from game.utils.Camera import Camera
+import pygame
+
+
+class TitleScreen(Scene):
+    def __init__(self, background : pygame.Surface):
+        pass
+
+    def update(self):
+
+        pass
+
+    def clearScene(self):
+        pass
