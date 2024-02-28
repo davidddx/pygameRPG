2f148093a9f71962c50d0ffc2f49ce013f36e139
davidddx <david0319072@gmail.com>
2024-02-28 14:09:43 -0500

    added Title Screen

--- game/SceneHandler.py
+++ game/SceneHandler.py
@@ -7,6 +7,7 @@ import importlib
 import pygame
 import gamedata.Save.SavedData as SAVED_DATA
 from game.Scenes.TitleScreen import TitleScreen
+import globalVars.SceneConstants as SCENE_CONSTANTS
 class SceneHandler:
     def __init__(self):
         self.scenes = ()
@@ -15,27 +16,48 @@ class SceneHandler:
         self.currentSceneIndex = 0
         self.SCENE_COUNT = 0
         self.timeLastChangedScene = pygame.time.get_ticks()
+        self.TitleScreen = None
         try:
             logger.debug(f"Class {SceneHandler=} initializing....")
-            self.scenes = self.loadScenes();
-            self.currentScene = self.loadCurrentScene(_type = SAVED_DATA.CURRENT_SCENE_TYPE, index= SAVED_DATA.CURRENT_SCENE_INDEX);
+            self.Areas = self.loadAreas();
+
+            self.TitleScreen = TitleScreen(background=pygame.image.load(os.getcwd() + '/images/test/titleScreenBackground.png'))
+            self.currentScene = self.TitleScreen
             logger.debug(f"Class {SceneHandler=} intialized.")
         except Exception as e:
             logger.error(f"Failed {SceneHandler=} class initialization.\n Error: {e}")
 
-    def getTitleScreen(self):
-        pass
 
-    def loadScenes(self):
-        pass
+    def loadAreas(self) -> list[Area]:
+        logger.info(f"Loading Areas........")
+        areas = []
+        try:
+            pass
+            # Code that loads areas
+        except Exception as e:
+            logger.error(f"Failed to load areas. Error: {e}")
+
+        logger.info(f"Loaded all Areas.")
+
+        areas.append(Area(name="test", map_idx=0))
+        return [Area(name="test", map_idx=0)]
+
+    def loadArea(self, idx : int) -> Area:
+        return self.Areas[idx];
 
     def loadCurrentScene(self, _type: str, index : int):
         if _type == PATH_CONSTANTS.AREA:
-            return Area(map_idx= index)
+            return Area(map_idx= index, name="Test")
         elif _type == PATH_CONSTANTS.TITLE_SCREEN:
-            cwd = os.getcwd()
-            titleScreenDir = cwd + '/images/titleScreenBackground.png'
-            return TitleScreen(image=pygame.image.load(titleScreenDir))
+            return self.TitleScreen
+
+    def checkSceneState(self, currentScene):
+        if not currentScene.state == SCENE_CONSTANTS.STATE_FINISHED:
+            return None
+        self.currentScene = 0
+        logger.debug(f"{currentScene=}")
+        self.currentScene = self.loadArea(SAVED_DATA.CURRENT_AREA_INDEX)
 
     def run(self, screen):
+        self.checkSceneState(currentScene=self.currentScene);
         self.currentScene.update(screen=screen)
