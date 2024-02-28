672d7b1dbf04117a975b717fd7011958026b1242
davidddx <david0319072@gmail.com>
2024-02-28 14:08:18 -0500

    added logging

--- game/utils/Button.py
+++ game/utils/Button.py
@@ -1,9 +1,10 @@
 import pygame
 import globalVars.SettingsConstants as GLOBAL_VARS
+from debug.logger import logger
 
 
 class ImagedButton:
-    def __init__(self, name : str, image : pygame.Surface, x : int, y : int, toggle=False, starting_value=False):
+    def __init__(self, name: str, image: pygame.Surface, x: int, y: int, toggle=False, starting_value=False):
         self.name = name;
         self.pressed = starting_value;
         self.toggle = toggle;
@@ -12,8 +13,8 @@ class ImagedButton:
         self.width = rect.width;
         self.height = rect.height
         self.hover = False;
-        self.x = x - self.width/2;
-        self.y = y - self.height/2;
+        self.x = x - self.width / 2;
+        self.y = y - self.height / 2;
 
     def checkMouseInRange(self, mouse_pos, button_x, button_y, button_width, button_height):
         mousePosX = mouse_pos[0]
@@ -28,20 +29,21 @@ class ImagedButton:
             return None;
         return True;
 
-    @staticmethod
-    def checkClicked(mouse_press):
+    def checkClicked(self, mouse_press):
         if mouse_press[0]:  # element 0 is left click
+            logger.debug(f"Button {self.name=}")
+
             return True;
         return None
 
-    def update(self, screen : pygame.Surface):
+    def update(self, screen: pygame.Surface):
         screen.blit(self.image, (self.x, self.y))
         if not self.checkMouseInRange(mouse_pos=pygame.mouse.get_pos(), button_x=self.x, button_y=self.y,
-                                  button_width=self.width, button_height=self.height):
+                                      button_width=self.width, button_height=self.height):
             self.hover = False
             return None
         self.hover = True
-        if ImagedButton.checkClicked(mouse_press=pygame.mouse.get_pressed()):
+        if self.checkClicked(mouse_press=pygame.mouse.get_pressed()):
             if not self.toggle:
                 self.pressed = True;
                 return None;
