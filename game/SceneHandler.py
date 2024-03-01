20f8e5ef372f06ec543d5c86c05d49e8c767f2cc
davidddx <david0319072@gmail.com>
2024-02-29 23:15:45 -0500

    added player

--- game/SceneHandler.py
+++ game/SceneHandler.py
@@ -8,19 +8,23 @@ import pygame
 import gamedata.Save.SavedData as SAVED_DATA
 from game.Scenes.TitleScreen import TitleScreen
 import globalVars.SceneConstants as SCENE_CONSTANTS
+from game.Player import Player
+
 class SceneHandler:
-    def __init__(self):
+    def __init__(self, _player : Player):
         self.scenes = ()
         self.Areas = None
+        self.player = None
         self.currentScene = None
+
         self.currentSceneIndex = 0
         self.SCENE_COUNT = 0
         self.timeLastChangedScene = pygame.time.get_ticks()
         self.TitleScreen = None
         try:
             logger.debug(f"Class {SceneHandler=} initializing....")
-            self.Areas = self.loadAreas();
-
+            self.Areas = self.loadAreas(_player= _player);
+            self.player = _player
             self.TitleScreen = TitleScreen(background=pygame.image.load(os.getcwd() + '/images/test/titleScreenBackground.png'))
             self.currentScene = self.TitleScreen
             logger.debug(f"Class {SceneHandler=} intialized.")
@@ -28,7 +32,7 @@ class SceneHandler:
             logger.error(f"Failed {SceneHandler=} class initialization.\n Error: {e}")
 
 
-    def loadAreas(self) -> list[Area]:
+    def loadAreas(self, _player) -> list[Area]:
         logger.info(f"Loading Areas........")
         areas = []
         try:
@@ -39,8 +43,8 @@ class SceneHandler:
 
         logger.info(f"Loaded all Areas.")
 
-        areas.append(Area(name="test", map_idx=0))
-        return [Area(name="test", map_idx=0)]
+        # areas.append(Area(name="test", map_idx=0))
+        return [Area(name="test", map_idx=0, _player= _player)]
 
     def loadArea(self, idx : int) -> Area:
         return self.Areas[idx];
