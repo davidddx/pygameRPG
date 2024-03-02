8646a02a5ba886299893a3412621016c13fed200
davidddx <david0319072@gmail.com>
2024-03-02 18:57:20 -0500

    not needed

--- game/utils/Camera.py
+++
@@ -1,6 +0,0 @@
-class Camera:
-    def __init__(self, pos : tuple[int, int]):
-        self.pos = pos
-
-    def changePos(self, x_factor : int, y_factor : int):
-        self.pos = (self.pos[0] + x_factor, self.pos[1] + y_factor);
