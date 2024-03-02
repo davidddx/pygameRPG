86f7c8058d976a28209f38d9ee7861c206fab331
davidddx <david0319072@gmail.com>
2024-03-02 18:56:48 -0500

    sprite edits/tiled

---
+++ gamedata/Maps/TileSets/Test.tsx
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Test" tilewidth="32" tileheight="32" tilecount="14" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteDown.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteDownLeft.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteDownRight.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteLeft.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteMiddle.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteRight.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteUp.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteUpLeft.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteUpRight.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/GrassSprite.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteCornerBottomRight.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteCornerTopLeft.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteCornerTopRight.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+  <image width="32" height="32" source="../../../images/test/DirtSpriteCornerBottomLeft.png"/>
+ </tile>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="Dirt" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="7" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="8" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="10" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="11" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="13" wangid="0,2,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
