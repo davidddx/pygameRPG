fa05353566ec0cc1a19c3ab8eb2870956044c0e4
davidddx <david0319072@gmail.com>
2024-04-24 00:21:09 -0400

    Removed try except finally

--- main/main.py
+++ main/main.py
@@ -2,10 +2,6 @@ from debug.logger import logger
 import game.GameHandler as gameHandler
 import sys
 if __name__ == '__main__':
-    try:
-        gameHandler.loop();
-        logger.info(f"Game method {gameHandler.loop=} has finished running.")
-    except Exception as e:
-        logger.error(f"Exception caught: {e}");
-    finally:
-        sys.exit()
+    gameHandler.loop();
+    logger.info(f"Game method {gameHandler.loop=} has finished running.")
+    sys.exit()
