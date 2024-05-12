98b57b4e50176b6f031928adeb113f9b01813786
davidddx <david0319072@gmail.com>
2024-05-12 00:05:08 -0400

    fixed plr part loading

--- game/Game.py
+++ game/Game.py
@@ -38,42 +38,5 @@ class Game:
             "pants" : pygame.image.load(f"{pantsDir}/PlrPants{saved_data.PLAYER_PANTS_ID}.png"),
             "arms" : pygame.image.load(f"{armsDir}/PlrArms{saved_data.PLAYER_ARMS_ID}.png"),
         }
-        plrParts = {
-            "player": pygame.image.load(f"{playerImageDir}/PlrSpriteReferenceNew.png")
-        }
 
-        #
-        # match saved_data.PLAYER_HEAD_ID:
-        #     case 0: pass
-        #     case 1: pass
-        #     case 2: pass
-        #     case 3: pass
-        # shirt = None
-        # match saved_data.PLAYER_SHIRT_ID:
-        #     case 0: shirt = pygame.image.load(shirtDir + "/PlrBody0.png")
-        #     case 1: pass
-        #     case 2: pass
-        #     case 3: pass
-        # feet = None
-        # match saved_data.PLAYER_FEET_ID:
-        #     case 0: feet = pygame.image.load(feetDir + "/PlrFeet.png")
-        #     case 1: pass
-        #     case 2: pass
-        #     case 3: pass
-        # arms = None
-        # match saved_data.PLAYER_ARMS_ID:
-        #     case 0: arms = pygame.image.load(armsDir + "/PlrArms.png")
-        #     case 1: pass
-        #     case 2: pass
-        #     case 3: pass
-        # hair = None
-        # match saved_data.PLAYER_HAIR_ID:
-        #     case 0: hair = pygame.image.load(hairDir + "/Bald.png")
-        #     case 1: pass
-        #     case 2: pass
-        #     case 3: pass
-        # match saved_data.PLAYER_EYES_ID:
-        #     case 0: pass
-        # match saved_data.PLAYER_EYEBROWS_ID:
-        #     case 0: pass
         return Player(pos= saved_data.PLAYER_POSITION, plrPartsDict=plrParts)
