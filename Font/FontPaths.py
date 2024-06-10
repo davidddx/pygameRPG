2bea20ff388dab1cc213971ae0d19f83e86d33a6
davidddx <david0319072@gmail.com>
2024-06-10 00:52:54 -0400

    added Fonts

---
+++ Font/FontPaths.py
@@ -0,0 +1,9 @@
+import os
+
+fontDir = os.path.join(os.getcwd(), "Font") 
+AGAVE = os.path.join(fontDir, "AgaveNerdFontMono-Regular.ttf")
+ANONYMICE_PRO = os.path.join(fontDir, "AnonymiceProNerdFontMono-Regular.ttf")
+CASKAYDIA = os.path.join(fontDir, "CaskaydiaMonoNerdFontMono-Regular.ttf")
+FIRA_CODE = os.path.join(fontDir, "FiraCodeNerdFontMono-Regular.ttf")
+GOHU = os.path.join(fontDir, "GohuFont14NerdFontMono-Regular.ttf")
+MONOFUR = os.path.join(fontDir, "MonofurNerdFontMono-Regular.ttf")
