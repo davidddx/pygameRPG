d6d73b452d66af3476c0dc4d4f16bf8fdad25c51
davidddx <david0319072@gmail.com>
2024-06-14 22:51:45 -0400

    created inventory system and added inventory adding

--- gamedata/playerdata/Inventory.py
+++ gamedata/playerdata/Inventory.py
@@ -2,11 +2,31 @@ import json
 import os
 from debug.logger import logger
 
+'''
+INVENTORY FORMAT
+(if player has no occurences of an item it will be taken out of the dict then they wont be seen in the dict)
+(each item is in one category only)
+
+
+{
+    "CATEGORY_1": {ITEM_1_ID: NUM_OCCURENCES, ... , ITEM_N_ID: NUM_OCCURENCES},
+    "CATEGORY_2": {ITEM_2_ID: NUM_OCCURENCES, ... , ITEM_M_ID: NUM_OCCURENCES},
+    ..... ,
+    ..... ,
+    ..... ,
+    "CATEGORY_N": {ITEM_3_ID: NUM_OCCURENCES, ... , ITEM_L_ID: NUM_OCCURENCES}
+}
+
+
+'''
+
+Inventory = {}
+
 def getInventoryPath():
     return os.path.join(os.getcwd(), "gamedata", "playerdata", "Inventory.json")
 
 def loadInventory() -> dict:
-    logger.info("loading inventory saved data...")
+    logger.info("Loading saved inventory data...")
     fileObj = open(getInventoryPath(), "r")
     return json.load(fileObj)
 
@@ -49,4 +69,36 @@ def saveInventoryData(inventory: dict):
 
     inventoryFile = open(path, "w")
     json.dump(inventory, inventoryFile) 
+
+def checkItemInInventory(item_id: int, item_category: str):
+    global Inventory
+    inventory = Inventory[item_category]
+    if item_id in inventory.keys():
+        return True
+    return False
+
+def addItemToInventory(item_id: int, item_category: str):
+    global Inventory
+    logger.debug(f"previous inventory before add {Inventory=}")
+    inventory = Inventory[item_category]
+    if not checkItemInInventory(item_id, item_category):
+        inventory[item_id] = 1
+    else:
+        inventory[item_id]+=1
+    Inventory[item_category] = inventory
+    logger.debug(f"inventory after add {Inventory=}")
+
+def removeItemFromInventory(item_id: int, item_category: str):
+    global Inventory
+    logger.debug(f"previous inventory before removal {Inventory=}")
+    inventory = Inventory[item_category]
+    if not checkItemInInventory(item_id, item_category):
+        logger.debug(f"error: {item_id=} not in {inventory.keys()=}")
+        return None
+    if inventory[item_id] == 1:
+        del inventory[item_id]
+    else:
+        inventory[item_id] -= 1
+    Inventory[item_category] = inventory
+    logger.debug(f"inventory after item removal {Inventory=}")
 
