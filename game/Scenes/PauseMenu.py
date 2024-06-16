378a3004c992efb7d634694034129d805f0509bf
davidddx <david0319072@gmail.com>
2024-06-16 18:47:03 -0400

    Scene fade in effect and test quit button

--- game/Scenes/PauseMenu.py
+++ game/Scenes/PauseMenu.py
@@ -1,21 +1,72 @@
 import pygame
 import os
-#from game.utils.Button import Button
+import Font.FontPaths as FONT_PATHS
+import globalVars.SettingsConstants as SETTINGS
+from game.utils.Button import Button, TextButton, TextAlignments
 from game.Scenes.BaseScene import Scene, SceneStates, SceneTypes
 class PauseMenu(Scene):
     def __init__(self, name: str, last_world_frame: pygame.Surface, time_last_paused):
-        self.name = name
+        super().__init__(name)
         self.state = SceneStates.INITIALIZING
-        self.lastWorldFrame = last_world_frame 
+        self.lastWorldFrame = last_world_frame
+        self.blackTransparentLayer = PauseMenu.loadBlackTransparentLayer(size= last_world_frame.get_size(), opacity=0)
         self.timeLastPaused = time_last_paused
-        self.state = SceneStates.RUNNING
+        self.transparentLayerOpacity = 0
+        self.timeLastAnimated = 0 
+        self.state = SceneStates.ON_ANIMATION
+        self.pausedFont = PauseMenu.turnStringToFontSurf(string = "PAUSE MENU", font_fp = FONT_PATHS.GOHU, base_size = SETTINGS.TILE_SIZE, anti_aliasing = True) 
+        self.pausedFontPos = (SETTINGS.SCREEN_WIDTH/2 - self.pausedFont.get_width() / 2, 0)
+
+        self.buttons = PauseMenu.generateButtons()
+
+    @staticmethod
+    def generateButtons() -> list[TextButton]:
+        quitButton = TextButton("QUIT", "Quit Button", x= SETTINGS.TILE_SIZE,y=  3 * SETTINGS.SCREEN_HEIGHT / 4, width= 3* SETTINGS.TILE_SIZE, height = 2*SETTINGS.TILE_SIZE, fit_to_text=True)
+
+        return [quitButton]
+
+
     def render(self, screen):
         screen.blit(self.lastWorldFrame, (0,0))
+        screen.blit(self.blackTransparentLayer, (0,0))
+        screen.blit(self.pausedFont, self.pausedFontPos)
+        for button in self.buttons:
+            button.update(screen)
+    @staticmethod
+    def turnStringToFontSurf(string: str, font_fp: str, base_size=24,  color= (255,255,255), anti_aliasing = False):        
+        return pygame.font.Font(font_fp, base_size).render(string, anti_aliasing, color)
+
+
+    def animate(self):
+        self.animateBackground(opacity= self.transparentLayerOpacity, time_last_animated = self.timeLastAnimated)
+
+    def animateBackground(self, opacity: int, time_last_animated):
+        cooldown = 10  # MILISECONDS 
+        opacityStep = 5
+        maxOpacity = 200
+        timenow = pygame.time.get_ticks()
+        if timenow - time_last_animated < cooldown:
+            return None
+        self.timeLastAnimated = timenow
+        opacity = self.transparentLayerOpacity
+        opacity += opacityStep
+        self.blackTransparentLayer = PauseMenu.loadBlackTransparentLayer(size= self.blackTransparentLayer.get_size(), opacity= opacity)
+        if opacity >= maxOpacity:
+            self.setState(SceneStates.RUNNING)
+        self.transparentLayerOpacity = opacity
 
     def update(self, screen):
+        if self.state == SceneStates.ON_ANIMATION:
+            self.animate()
         self.checkUnpauseSignal()
         self.render(screen)
-    
+   
+    @staticmethod
+    def loadBlackTransparentLayer(size: tuple, opacity: int):
+        surf = pygame.Surface(size, pygame.SRCALPHA)
+        surf.fill((0,0,0,opacity))
+        return surf
+
     def getTimeLastPaused(self): return self.timeLastPaused
 
     def checkUnpauseSignal(self):
