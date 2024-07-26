f1f3cc32c3a14835fd626612c63ed89997f0626e
davidddx <david0319072@gmail.com>
2024-07-26 17:11:47 -0400

    logging to stdout

--- debug/logger.py
+++ debug/logger.py
@@ -1,16 +1,18 @@
 import logging
-
+import sys
 logfile = "projectDebug.log"
 with open(logfile, 'w'):  # truncates file if there is contents in it
     pass
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR
-                )
-handler = logging.FileHandler(logfile)
+logger.setLevel(logging.DEBUG)
+fileHandler = logging.FileHandler(logfile)
+consoleHandler = logging.StreamHandler(sys.stdout)
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
 
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+fileHandler.setFormatter(formatter)
+consoleHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.addHandler(consoleHandler)
 try:
     logger.info('LOGGER HAS BEEN INITIALIZED')
 except Exception as e:
