79884a1654bba29d1331e6842e8897820daad558
davidddx <david0319072@gmail.com>
2024-09-09 00:07:51 -0400

    doing this later

---
+++ gamedata/Moves.py
@@ -0,0 +1,38 @@
+'''
+MOVE FORMAT:
+MOVE_NAME = {
+    NAME : "MOVE_NAME",
+    TYPE : MoveTypes.MOVE_TYPE,
+    DMG : DMG_VAL,
+    LUCK : LUCK_VAL
+
+    
+}
+'''
+NAME = 'NAME'
+DMG = 'DAMAGE'
+LUCK = 'LUCK'
+TYPE = 'TYPE'
+class MoveTypes:
+    STATUS_EFFECT = "STATUS_EFFECT"
+    MELEE = "MELEE"
+    RANGED = "RANGED"
+
+PUNCH = {
+        NAME: "PUNCH",
+        TYPE: MoveTypes.MELEE,
+        DMG: 5,
+        LUCK: 10,
+}
+HEADBUTT = {
+        NAME: "HEADBUTT",
+        TYPE: MoveTypes.MELEE,
+        DMG: 7,
+        LUCK: 0,
+}
+KICK = {
+        NAME: 'KICK',
+        TYPE: MoveTypes.MELEE,
+        DMG: 7,
+        LUCK: 5,
+}
