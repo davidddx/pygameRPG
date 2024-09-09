b2daa96d5bf2988d8e4bf2fa17cb345ff303c113
davidddx <david0319072@gmail.com>
2024-09-09 00:07:02 -0400

    math function and i think camera zoom

--- game/utils/SettingsFunctions.py
+++ game/utils/SettingsFunctions.py
@@ -4,12 +4,58 @@ import json
 import gamedata.Save.SavedData as SAVED_DATA
 import Font.FontPaths as FONT_PATHS
 import pathlib
+from debug.logger import logger
 
 def checkVariableInClass(variable_value, _class) -> bool:
     return True if hasattr(_class, variable_value) else False
 
 def getIcon(cwd: str) -> pygame.Surface: 
     return pygame.image.load(os.path.join(cwd, "icon", "icon.png"))
+
+#this function assumes that param blitted_surface already has everything drawn on it for the frame.
+# screen: destination screen
+# blitted surface: zooming screen 
+# rect: rect to zoom in to
+# zoom_scale: scale of zoom
+def zoomToSurface(screen: pygame.Surface, blitted_surface: pygame.Surface, rect: pygame.Rect, zoom_scale: float, position_transition_scalar=1):
+    blitted_surface = pygame.transform.smoothscale_by(blitted_surface, zoom_scale)
+    blittedSurfaceRect = blitted_surface.get_rect()
+    screenRect = screen.get_rect()
+    blittedSurfaceRect.x = (screenRect.centerx - rect.centerx * zoom_scale) * position_transition_scalar
+    blittedSurfaceRect.y = (screenRect.centery - rect.centery * zoom_scale) * position_transition_scalar
+    screen.blit(blitted_surface, blittedSurfaceRect)
+
+def zoomToPosition(screen: pygame.Surface, blitted_surface:pygame.Surface, position1, position2, zoom_scale: float, position_transition_scalar=1):
+    
+    '''
+    ex:
+    (x1, y1) to (x2, y2)
+    (0,0) to (30,50), numsteps = 5
+    (6, 10), (12, 20), (18, 30), (24, 40), (30,50)
+    (x = (x2-x1)/5, y=(y2 - y1)/5))
+    but user controls position transition scalar
+    '''
+    logger.debug(f"{position_transition_scalar=}")
+    logger.debug(f"{position2=}")
+    logger.debug(f"{position1=}")
+    blitted_surface = pygame.transform.smoothscale_by(blitted_surface, zoom_scale)
+    scaleOffsetX = screen.get_width()/2 * position_transition_scalar
+    scaleOffsetY = screen.get_height()/2 * position_transition_scalar
+    blittedSurfX = (position2[0]- position1[0] ) * position_transition_scalar  * zoom_scale - scaleOffsetX
+
+    blittedSurfY = (position2[1] - position1[1] ) * position_transition_scalar * zoom_scale - scaleOffsetY
+
+    #logger.debug(f"{(blittedSurfX, blittedSurfY)=}")
+    '''
+    center of goal position on bottom left
+    '''
+
+    screen.blit(blitted_surface, (blittedSurfX, blittedSurfY))
+    #screen.blit(blitted_surface, (0,0))
+    pass
+
+
+    
 
 def generateScreenFromResolution(width:int, height:int, screen_size: str) -> pygame.Surface:
     if screen_size == "FULLSCREEN": return pygame.display.set_mode((0,0), pygame.FULLSCREEN)
