d7b92bbd40277c8b154fdbda9a6c78e41c15210f
davidddx <david0319072@gmail.com>
2024-11-07 17:48:37 -0500

    set variable PLAYER as name var across project & some other stuff i forgot

--- game/Player.py
+++ game/Player.py
@@ -4,18 +4,7 @@ import globalVars.SettingsConstants as globalVars
 import gamedata.Save.SavedData as SAVED_DATA
 import gamedata.Save.PlayerCustomization as PLAYER_CUSTOMIZATION
 
-class MinimalPart(pygame.sprite.Sprite):
-    NONE = "NONE"
-    def __init__(self, group: pygame.sprite.Group, name: str, image):
-        super().__init__(group)
-        self.name = name
-        self.image = image
-
-    def render(self, position: tuple, screen: pygame.Surface):
-        if self.image is None:
-            return None
-        screen.blit(self.image, position)
-
+PLAYER = "PLAYER"
 
 class DirectionNames:
     FRONT = "Front"
